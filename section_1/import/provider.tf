4b5d12186d738b7085146c6447e724bb817e5b80
julian1059 <jmmaze@outlook.com>
2025-01-12 18:07:31 -0800

    refactor: section_1 >> section_2

--- section_1/import/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
-import {
-  to = aws_route53_zone.julianmaze
-  id = "Z0407921W5XGEPSJT29J"
-}
--- section_1/moved_blocks/moved.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_group" "payment_database_firewall" {
-  name = "db_firewall"
-}
-
-
-moved {
-  from = aws_security_group.database_firewall
-  to   = aws_security_group.payment_database_firewall
-}
